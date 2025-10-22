048c9ac4f13601f8afc1b9b33080b0cbb7e6b798
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-10-22 09:41:29 +0800

    UI functions

--- ES-Exhibit/indigenous tactical comms/include/ui.hpp
+++ ES-Exhibit/indigenous tactical comms/include/ui.hpp
@@ -1,23 +1,25 @@
 #pragma once
+#include "app_config.h"
 #include <Arduino.h>
 
-// Screen state (same names/values)
+// UI state screens
 enum ScreenState { SCREEN_HOME = 0, SCREEN_RX = 1, SCREEN_LOG_LIST = 2, SCREEN_LOG_VIEW = 3, SCREEN_COMPOSE = 4 };
 extern ScreenState screen;
+extern uint8_t logListPage;
+extern String lastRx;
+extern String rxFull;
 
-// Shared cadence timer from original sketch
-extern uint32_t lastSave;
-
-// Compose namespace API (same names)
-namespace Compose {
-  void enter();
-  void render();
-  void handleKey(char k);
-  void autoCommitIfTimeout();
-}
-
-// Home helpers used by keypad
+// UI screens and interface
+void drawHome();
+void drawRxScreen();
+void drawLogListScreen();
+void drawLogViewScreen(int selectedSlot);
 String buildMessage(uint8_t which);
 
-// Keypad route
-void handleKeypad();
+// Compose (custom message composer) namespace interface
+namespace Compose {
+    void handleKey(char k);
+    void autoCommitIfTimeout();
+    void render();
+    void enter();
+}
