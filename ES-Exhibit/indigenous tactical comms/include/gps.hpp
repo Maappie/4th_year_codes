6a2b0f59427aaa4ea31931215400c1c297442888
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-10-22 09:40:20 +0800

    gps functions

--- ES-Exhibit/indigenous tactical comms/include/gps.hpp
+++ ES-Exhibit/indigenous tactical comms/include/gps.hpp
@@ -1,26 +1,24 @@
 #pragma once
-#include <Arduino.h>
+#include "app_config.h"
 #include <TinyGPSPlus.h>
 
-// Exposed globals/state
+// GPS and location management
 extern TinyGPSPlus gps;
 extern HardwareSerial GPSSerial;
-
-extern double curLat, curLng;
+extern double curLat;
+extern double curLng;
 extern uint32_t curSat;
 extern double curHdop;
 extern uint32_t lastFixMillis;
+extern double nvsLat;
+extern double nvsLng;
 
-extern double nvsLat, nvsLng;
-
-// NMEA batching (same behavior)
-void printNMEADump1Hz();
-
-// API
-void loadNVS();
+// GPS / NVS (GPS) function declarations
+void loadNVS();             // Load last saved GPS coords from NVS
 void saveNVS(double lat, double lng);
 bool haveLiveFix();
 void updateLiveFix();
+void printNMEADump1Hz();
 void handleGPSStream();
-String formatLocation(double lat, double lng);
 void maybePersistFix();
+String formatLocation(double lat, double lng);
